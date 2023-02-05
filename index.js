4ba0bb2f51a9829ec9dfd0e3ade56198376a249f
AlexDeveloper12 <angrycoder95@gmail.com>
2023-02-05 16:14:42 +0000

    Adding change for cities endpoint

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var axios = require('axios');
+const { response } = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
 require('dotenv').config()
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.send({
-        message:"Default route"
+        message: "Default route"
     });
 });
 
-app.get("/currentweather/:lat/:lon",function(req,res){
-    //https://api.openweathermap.org/data/2.5/weather?
+app.get("/currentweather/:lat/:lon", function (req, res) {
     const lat = req.params.lat;
     const lon = req.params.lon;
     axios.get(`${process.env.OPEN_WEATHER_LINK}?lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`)
-    .then(response=>{
-        res.send({
-            message:response.data
-        });
-    })
+        .then(response => {
+            res.send({
+                message: response.data
+            });
+        })
 })
 
-app.get("/forecast/:lat/:lon",function(req,res){
+app.get("/forecast/:lat/:lon", function (req, res) {
     const lat = req.params.lat;
     const lon = req.params.lon;
 
     axios.get(`${process.env.OPEN_WEATHER_FORECAST_LINK}?lat=${lat}&lon=${lon}&units=imperial&cnt=5&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`)
-    .then(response=>{
-        res.send({
-            message:response.data
-        });
-    })
-
-//https://api.openweathermap.org/data/2.5/forecast/daily?
+        .then(response => {
+            res.send({
+                message: response.data
+            });
+        })    
 });
 
-app.get("/cities",function(req,res){
-//https://wft-geo-db.p.rapidapi.com/v1/geo
+app.get("/cities/:city", function (req, res) {
+    
+    const city = req.params.city;
 
-
+    axios.get(`${process.env.GEODB_CITIES_LINK}/cities?minPopulation=1000000&namePrefix=${city}`)
+        .then(response => {
+            res.send({
+                message: response.data
+            })
+        })
 
 });
 
 
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
